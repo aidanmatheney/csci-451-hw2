a828e3eeccdc91154f8166a2dcf07514289cc064
Aidan Matheney <aidanmatheney@gmail.com>
2020-09-04 15:09:02 -0500

    Initial commit

---
+++ src/hw2-aidanmatheney.c
@@ -0,0 +1,16 @@
+/*
+ * Aidan Matheney
+ * aidan.matheney@und.edu
+ *
+ * CSCI 451 HW2
+ */
+
+#include "../include/hw2.h"
+
+#include <stdlib.h>
+
+int main(void) {
+    hw2();
+
+    return EXIT_SUCCESS;
+}
---
+++ src/hw2.c
@@ -0,0 +1,10 @@
+#include "../include/hw2.h"
+
+#include <stdio.h>
+
+/**
+ * Run CSCI 451 HW2.
+ */
+void hw2(void) {
+    puts("HW2: Hello world");
+}
